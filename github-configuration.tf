30761035c6118aeefc1d2f7d815442f65cfd673b
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-06-27 14:26:58 +0100

    feat: apply branch protection to repositories

--- github-configuration.tf
+++ github-configuration.tf
@@ -12,17 +12,46 @@ variable GITHUB_TOKEN {
   description = "GitHub token required for authentication"
 }
 
+variable "OPENSTACK_REPOSITORIES" {
+  default = [
+    "kolla",
+    "kolla-ansible"
+  ]
+}
 
 provider "github" {
   token = var.GITHUB_TOKEN
-  owner = "jackhodgkiss"
+  owner = "a-demo-org"
 }
 
-data "github_branch" "default" {
-  repository = "kolla"
-  branch = "master"
+resource "github_branch_protection" "OPENSTACK_REPOSITORIES" {
+
+  for_each = toset(var.OPENSTACK_REPOSITORIES)
+  repository_id = each.key
+
+  pattern = "stackhpc/**"
+  require_conversation_resolution = true
+  allows_deletions = false
+  allows_force_pushes = false
+
+  push_restrictions = [
+    data.github_user.jackhodgkiss.node_id,
+  ]
+
+  required_status_checks {
+    contexts = [
+      "tox / Tox pep8 with Python 3.8",
+      "tox / Tox py36 with Python 3.6",
+      "tox / Tox py38 with Python 3.8",
+    ]
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    require_code_owner_reviews = true
+  }
 }
 
-output "branch" {
-  value = "${data.github_branch.default}"
+data "github_user" "jackhodgkiss" {
+  username = "jackhodgkiss"
 }
