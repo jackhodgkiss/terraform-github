eb30f274828aa09db03030d1274e1c4e2718be03
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-06-30 09:45:37 +0100

    feat: split components into separate files

---
+++ teams.tf
@@ -0,0 +1,29 @@
+resource "github_team" "organisation_teams" {
+  for_each = var.teams
+  name = "${each.key}"
+  description = "${each.value.description}"
+  privacy = "${each.value.privacy}"
+}
+
+resource "github_team_members" "team_membership" {
+  for_each = {
+    for team in resource.github_team.organisation_teams: team.id => 
+      var.teams[team.name].users
+  }
+  team_id = "${each.key}"
+  dynamic "members" {
+    for_each = "${each.value["maintainers"]}"
+    content {
+      username = "${members.value}"
+      role = "maintainer"
+    }
+  }
+
+  dynamic "members" {
+    for_each = "${each.value["members"]}"
+    content {
+      username = "${members.value}"
+      role = "member"
+    }
+  }
+}
